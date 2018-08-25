f3977b067c4bf3d54538d7040cab5156c6f69f7a
bpicode <bjoern.pirnay@googlemail.com>
2018-08-25 12:21:54 +0200

    Formula: fritzctl v1.4.23

--- Formula/fritzctl.rb
+++ Formula/fritzctl.rb
@@ -1,9 +1,9 @@
 class Fritzctl < Formula
   desc "A lightweight, easy to use console client for the AVM FRITZ!Box Home Automation"
   homepage "https://github.com/bpicode/fritzctl"
-  url "https://github.com/bpicode/fritzctl/releases/download/v1.4.20/fritzctl-1.4.20-darwin-amd64.zip"
-  version "1.4.20"
-  sha256 "ba25c29cbfe1566a17b1f38fb51ae1a85d4672a6add1b7f9ef8a212fa75e154a"
+  url "https://github.com/bpicode/fritzctl/releases/download/v1.4.23/fritzctl-1.4.23-darwin-amd64.zip"
+  version "1.4.23"
+  sha256 "69f5a32bb0d3a0ad1f5781be98cb93ecdd86d151d788e7f0a3a0610a5de16b2d"
 
   def install
     bin.install "build/distibutions/darwin_amd64/fritzctl"
